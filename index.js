0604b05e2cb33809f712ce40b1a0549922e1b471
Hosein Khansari <hrk.x11@gmail.com>
2021-04-13 11:43:59 +0430

    Accept string inputs

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const char = x => x.charCodeAt(0);
 const OFFSET = char('۱') - char('1');
 
 /**
+ * @param {number|string} number
  * @param {Object} options
  * @param {boolean=} options.ignorePeriods If true, periods are not converted to slashes. (default: false)
  * @param {boolean=} options.convertZeroToWord If true, returns صفر instead of ۰ (default: true)
  * @returns {string}
 */
 export function toFarsiDigits(number, { ignorePeriods = false, convertZeroToWord = true } = {}) {
-  if (convertZeroToWord && number === 0) return 'صفر';
+  if (convertZeroToWord && (number === 0 || number === '0')) return 'صفر';
   return number.toString()
     .split('')
     .map(digit => {
